9e2e890284cc2a8beedac8b77ae0b9bbab920a6d
shyamprgrmr <2018bcs509@sggs.ac.in>
2024-11-30 00:29:27 +0530

    output-variable

---
+++ 7-output-variables/main.tf
@@ -0,0 +1,30 @@
+
+variable static_content {
+  type        = string
+  default     = "My pet's name is"
+}
+
+resource "random_pet" "pet" {
+    prefix="PET"
+    separator="_"
+    length=1
+}
+
+resource "local_file" "file" {
+    filename = "D://Terraform_Practice/7-output-variables/file.txt"
+    content = "${var.static_content} ${random_pet.pet.id}" 
+}
+
+output "output-var" {
+    value = local_file.file.content
+    description = "Example of output variable!"
+}
+
+
+/*
+Output : 
+terraform init
+terraform plan
+terraform apply
+output-var = "My pet's name is PET_crab"
+*/
